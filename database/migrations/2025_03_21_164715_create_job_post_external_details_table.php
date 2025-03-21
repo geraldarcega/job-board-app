3bbe6cc6c8942008180119a0d11db96d9a807b7d
Michael Gerald Arcega <michaelgeraldarcega@gmail.com>
2025-03-22 00:57:04 +0800

    add table for external jobs

---
+++ database/migrations/2025_03_21_164715_create_job_post_external_details_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job_post_external_details', function (Blueprint $table) {
+            $table->bigInteger('id')->unsigned()->primary();
+            $table->string('subcompany', 150)->nullable();
+            $table->string('office', 150)->nullable();
+            $table->string('department', 150)->nullable();
+            $table->string('recruiting_category', 150)->nullable();
+            $table->string('seniority', 50)->nullable();
+            $table->string('schedule', 50)->nullable();
+            $table->string('yearsOfExperience', 10)->nullable();
+            $table->string('keywords')->nullable();
+            $table->string('occupation', 150)->nullable();
+            $table->string('occupationCategory', 150)->nullable();
+            $table->json('job_descriptions')->nullable();
+            $table->json('additional_offices')->nullable();
+            $table->timestamp('created_at');
+        });
+
+        Schema::table('job_posts', function (Blueprint $table) {
+            $table->bigInteger('external_id')->unsigned()->nullable();
+            $table->foreign('external_id')
+                ->references('id')
+                ->on('job_post_external_details');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_post_external_details');
+    }
+};
